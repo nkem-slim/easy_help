ca9320f1104a5d6f0a9844be216d0717f3abe78f
Nkemakolamuko <nwekevino99@gmail.com>
2026-07-30 19:05:22 +0200

    chore: connect firebase and set up auth using google signin and also Email/password signin method

--- lib/core/di/injection_container.dart
+++ lib/core/di/injection_container.dart
@@ -1,8 +1,9 @@
-// import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:get_it/get_it.dart';
-// import 'package:google_sign_in/google_sign_in.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 import '../network/network_info.dart';
 import '../../features/auth/data/datasources/auth_remote_data_source.dart';
@@ -12,7 +13,6 @@ import '../../features/auth/domain/usecases/login_usecase.dart';
 import '../../features/auth/domain/usecases/logout_usecase.dart';
 import '../../features/auth/domain/usecases/register_usecase.dart';
 import '../../features/auth/presentation/bloc/auth_bloc.dart';
-import '../../features/auth/data/models/user_model.dart';
 
 import '../../features/appointments/data/datasources/appointment_remote_data_source.dart';
 import '../../features/appointments/data/models/appointment_model.dart';
@@ -31,34 +31,6 @@ import '../../features/screening/domain/usecases/submit_screening_usecase.dart';
 import '../../features/screening/presentation/bloc/screening_bloc.dart';
 
 final GetIt sl = GetIt.instance;
-
-// TODO: remove stub and uncomment Firebase registrations when Firebase is configured
-class _AuthRemoteDataSourceStub implements AuthRemoteDataSource {
-  @override
-  Future<UserModel> loginWithEmail({
-    required String email,
-    required String password,
-  }) => throw UnimplementedError('Firebase not yet initialized');
-
-  @override
-  Future<UserModel> registerWithEmail({
-    required String name,
-    required String email,
-    required String password,
-    required String role,
-  }) => throw UnimplementedError('Firebase not yet initialized');
-
-  @override
-  Future<UserModel> signInWithGoogle() =>
-      throw UnimplementedError('Firebase not yet initialized');
-
-  @override
-  Future<void> logout() =>
-      throw UnimplementedError('Firebase not yet initialized');
-
-  @override
-  Stream<UserModel?> get authStateChanges => const Stream.empty();
-}
 
 // TODO: remove stub and uncomment Firebase registrations when Firebase is configured
 class _AppointmentRemoteDataSourceStub implements AppointmentRemoteDataSource {
@@ -106,9 +78,17 @@ class _ScreeningRemoteDataSourceStub implements ScreeningRemoteDataSource {
 }
 
 Future<void> initDependencies() async {
-  // sl.registerLazySingleton(() => FirebaseAuth.instance);
-  // sl.registerLazySingleton(() => FirebaseFirestore.instance);
-  // sl.registerLazySingleton(() => GoogleSignIn());
+  sl.registerLazySingleton(() => FirebaseAuth.instance);
+  sl.registerLazySingleton(() => FirebaseFirestore.instance);
+  sl.registerLazySingleton(
+    () => GoogleSignIn(
+      // Web (unlike Android/iOS) has no google-services.json/plist to read
+      // the OAuth client id from, so it must be passed explicitly here.
+      clientId: kIsWeb
+          ? '673918271872-vpn9gscug1bgfqj90hgqjg1d84kk3ll7.apps.googleusercontent.com'
+          : null,
+    ),
+  );
   sl.registerLazySingleton(() => Connectivity());
 
   sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(sl()));
@@ -120,13 +100,11 @@ Future<void> initDependencies() async {
 
 void _initAuth() {
   sl.registerLazySingleton<AuthRemoteDataSource>(
-    // TODO: swap stub for real impl when Firebase is configured
-    () => _AuthRemoteDataSourceStub(),
-    // () => AuthRemoteDataSourceImpl(
-    //   firebaseAuth: sl(),
-    //   firestore: sl(),
-    //   googleSignIn: sl(),
-    // ),
+    () => AuthRemoteDataSourceImpl(
+      firebaseAuth: sl(),
+      firestore: sl(),
+      googleSignIn: sl(),
+    ),
   );
 
   sl.registerLazySingleton<AuthRepository>(
